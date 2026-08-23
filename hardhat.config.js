6b9c1e5ea0ed8b2b127a4d0443f5c060da7a2eac
Muhammad Umar <umarkohati01@gmail.com>
2026-08-23 18:56:15 +0500

    Align public Hardhat networks with production paymaster scripts

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,6 +11,14 @@ module.exports = {
   },
   networks: {
     hardhat: {},
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "",
+      accounts: process.env.PAYMASTER_DEPLOYER_PRIVATE_KEY ? [process.env.PAYMASTER_DEPLOYER_PRIVATE_KEY] : [],
+    },
+    bsc: {
+      url: process.env.BSC_DEPLOY_RPC_URL || "https://bsc-rpc.publicnode.com",
+      accounts: process.env.PAYMASTER_DEPLOYER_PRIVATE_KEY ? [process.env.PAYMASTER_DEPLOYER_PRIVATE_KEY] : [],
+    },
     bscTestnet: {
       url: process.env.BSC_TESTNET_RPC_URL || "https://bsc-testnet-rpc.publicnode.com",
       accounts: process.env.BSC_TESTNET_DEPLOYER_PRIVATE_KEY
